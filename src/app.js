edbfe3eff9196fab13bacc6f96b2260719f3b700
Hyewon Kim <hkim444@my.bcit.ca>
2022-04-08 20:41:54 -0700

    added save and delete buttons

--- src/app.js
+++ src/app.js
@@ -15,3 +15,25 @@ plusButton.addEventListener('click', (e) => {
       addNoteArea();
     }
 });
+
+function addNoteArea() {
+  const noteTakingArea = document.createElement('section');
+
+  const textarea = document.createElement('textarea');
+  const cancel = document.createElement('button');
+  const save = document.createElement('button');
+
+  cancel.innerHTML = 'cancel';
+  cancel.addEventListener('click', (e) => {removeArea(e.target)});
+  save.innerHTML = 'save'; 
+  
+  textarea.innerHTML ="Write title \nWrite something here...";
+
+  save.addEventListener('click', (e) => {addNote(e.target)});
+  
+  noteTakingArea.appendChild(textarea);
+  noteTakingArea.appendChild(save);
+  noteTakingArea.appendChild(cancel);
+
+  createNoteArea.appendChild(noteTakingArea);
+}
