509320ddf813716d345ea8c0b33fb144ec24a9b8
Hyewon Kim <hkim444@my.bcit.ca>
2022-04-08 20:40:06 -0700

    when plus sign clicked, new note taking area added below button

--- src/app.js
+++ src/app.js
@@ -5,3 +5,13 @@ const notes = [
     id: 1 
   }
 ]
+
+const createNoteArea = document.querySelector('.create-note-area');
+const plusButton = document.querySelector('.icons');
+
+plusButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (createNoteArea.children.length <= 1) {
+      addNoteArea();
+    }
+});
